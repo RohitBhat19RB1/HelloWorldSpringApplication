f885ba772e21030384096f25c625b811ab25b24b
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2021-01-15 00:08:13 +0530

    Added:Methods for GET,POST and PUT operations

--- src/main/java/com/example/demo/Controller/HelloRESTController.java
+++ src/main/java/com/example/demo/Controller/HelloRESTController.java
@@ -1,7 +1,12 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.User;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,22 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRESTController {
 
 	// curl localhost:8080/hello -w "\n"
-	@RequestMapping(value = { "", "/", "/home","/roit" })
+	@RequestMapping(value = { "", "/", "/home" })
 	public String sayHello() {
 		return "Hello World!";
 	}
 
 	// curl localhost:8080/hello/query?name=Pratik -w "\n"
-	// curl -X GET "http://localhost:8080/hello/query/?fName=Pratik&lName=Valvi" -w "\n"
+	// curl -X GET "http://localhost:8080/hello/query/?fName=Rohit&lName=Bhat" -w "\n"
 	@RequestMapping(value = { "/query" }, method = RequestMethod.GET)
 	public String sayHello(@RequestParam(value = "fName") String fName, @RequestParam(value = "lName") String lName) {
 		return "Hello " + fName + " " + lName + " !";
 	}
 
-	// curl -X GET "http://localhost:8080/hello/param/Pratik" -w "\n"
+	// curl -X GET "http://localhost:8080/hello/param/Rohit" -w "\n"
 	@GetMapping("/param/{name}" )
 	public String sayHelloParam(@PathVariable String name) {
 			return "Hello "  + name + "!";
 	}
-	
+
+	// curl -X POST -H "Content-Type: application/json" -d 
+	// '{"firstName": "Lisa", "lastName": "Harry"}'
+	// "http://localhost:8080/hello/post" -w "\n"
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+			return "Hello " + user.getFirstName() + " " + user.getLastName() + '!';
+	}
+
+	// curl -X PUT localhost:8080/hello/put/Lisa/?lastName=Terrisa -w "\n"
+	@PutMapping("/put/{firstName}")
+	public String sayHelloo(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+			return "Hello " + firstName + " " + lastName + '!';
+	}
 }
